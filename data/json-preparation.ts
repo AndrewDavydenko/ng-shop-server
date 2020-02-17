021fecf75402d30b917ca2eeee2e6439b1757f08
Ihor <igornepipenko@gmail.com>
2020-02-17 18:40:58 +0200

    Feature: start work with data parser [ref:no-ref]

--- data/json-preparation.ts
+++ data/json-preparation.ts
@@ -9,9 +9,32 @@ interface IBaseCategory {
   weight: number;
 }
 
+interface IBaseSubcategory {
+  id: string;
+  title: string;
+  count: number;
+  category: string;
+  weight: number;
+}
+
 interface ICategory {
   _id: mongoose.Types.ObjectId;
   name: string;
+}
+
+interface ISubCategory {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+}
+
+interface IProduct {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+}
+
+interface IJsonData {
+  categories: IBaseCategory[];
+  subcategories: IBaseSubcategory[];
 }
 
 const fileName = 'db-base.json';
@@ -22,17 +45,29 @@ async function main() {
   try {
     const asyncFileReader = util.promisify(fs.readFile);
     const buffer: Buffer = await asyncFileReader(`${__dirname}/${fileName}`);
-    const json = JSON.parse(buffer.toString()) as { categories: IBaseCategory[] };
-    // tslint:disable-next-line:no-any
-    const categories: ICategory[] =
-      json.categories.map(({ title }: IBaseCategory): ICategory => {
-        const _id: mongoose.Types.ObjectId = mongoose.Types.ObjectId();
-        // TODO work with subcategory
-        // json.subcategory
-        return { name: title, _id };
-      });
+    const json: IJsonData = JSON.parse(buffer.toString()) as IJsonData;
+    let subCategories: ISubCategory[] = [];
+    const products: IProduct[] = [];
+    const categories: ICategory[] = json.categories.map(
+      ({ title, id }: IBaseCategory): ICategory => {
+        const categoryId: mongoose.Types.ObjectId = mongoose.Types.ObjectId();
+        // subCategories
+        const subcategories = json.subcategories
+          .filter(({ category }) => id === category)
+          .map(({ title: subTitle }: IBaseSubcategory) => {
+            const _subId: mongoose.Types.ObjectId = mongoose.Types.ObjectId();
+            return { name: subTitle, _id: _subId, category: categoryId };
+          });
+        subCategories = [...subCategories, ...subcategories];
+        return { name: title, _id: categoryId };
+      }
+    );
     // tslint:disable-next-line:no-console
     console.log(categories);
+    // tslint:disable-next-line:no-console
+    console.log(subCategories);
+    // tslint:disable-next-line:no-console
+    console.log(products);
   } catch (err) {
     // tslint:disable-next-line:no-console
     console.log(err);
