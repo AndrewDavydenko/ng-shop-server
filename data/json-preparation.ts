48fbd08374bce1109d1543638223556fecaf5024
Ihor <igornepipenko@gmail.com>
2020-02-17 18:28:57 +0200

    Feature: start work with data parser [ref:no-ref]

--- data/json-preparation.ts
+++ data/json-preparation.ts
@@ -1,5 +1,18 @@
 import * as fs from 'fs';
 import * as util from 'util';
+import * as mongoose from 'mongoose';
+
+interface IBaseCategory {
+  id: string;
+  title: string;
+  count: number;
+  weight: number;
+}
+
+interface ICategory {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+}
 
 const fileName = 'db-base.json';
 
@@ -9,12 +22,19 @@ async function main() {
   try {
     const asyncFileReader = util.promisify(fs.readFile);
     const buffer: Buffer = await asyncFileReader(`${__dirname}/${fileName}`);
-    const json = JSON.parse(buffer.toString());
-    json.categories?.map((category: any) => {
-      console.log(category);
-    });
+    const json = JSON.parse(buffer.toString()) as { categories: IBaseCategory[] };
+    // tslint:disable-next-line:no-any
+    const categories: ICategory[] =
+      json.categories.map(({ title }: IBaseCategory): ICategory => {
+        const _id: mongoose.Types.ObjectId = mongoose.Types.ObjectId();
+        // TODO work with subcategory
+        // json.subcategory
+        return { name: title, _id };
+      });
+    // tslint:disable-next-line:no-console
+    console.log(categories);
   } catch (err) {
+    // tslint:disable-next-line:no-console
     console.log(err);
   }
 }
-
